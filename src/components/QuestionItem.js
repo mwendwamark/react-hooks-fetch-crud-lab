d5e8c88dce9a7355b062ba62a5a636459a6ab731
mark mwendwa <mwendwamark310@gmail.com>
2023-04-25 16:48:00 +0300

    completed

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question,content, reset}) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,41 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDelete(id){
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "DELETE",
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(response=>response.json())
+    .then(()=>{
+      let deleted = content.filter(question => question.id !== id)
+      reset(deleted)
+    })
+    .catch(error=>console.log(error))
+  }
+
+  function handleCorrectAnswer(e){
+    console.log(id);
+    let answer = e.target.value
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "PATCH",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({correctIndex: answer})
+    })
+    .catch(error=>console.log(error))
+  }
+    
+  
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleCorrectAnswer}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={()=>handleDelete(id)}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  const [originalContent, setOriginalContent] = useState([])
+
+  useEffect(()=>{
+
+      fetch("http://localhost:4000/questions")
+      .then(response => response.json())
+      .then(data => {
+        setOriginalContent(data)        
+      })
+
+  },[])
+
+  const questions = originalContent.map(question => {
+    return(
+      <>
+        <QuestionItem question={question} key={question.id} content={originalContent} reset={setOriginalContent}/>
+      </>
+    )
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions}</ul>
     </section>
   );
 }
